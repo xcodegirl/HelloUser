7f9afcc29dd8ea4f7c922607c3b7d4c37a484c76
Jo Hoar <joanne.hoar@gmail.com>
2025-02-07 11:42:41 -0700

    Initial commit csharp demo console app

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// HelloUser main routine
+// by Joanne
+
+using System;
+
+class HelloUser{
+	
+	static void Main()
+	{
+		string name;
+		
+		Console.WriteLine("What is your name?");
+
+		name = Console.ReadLine();		
+
+		Console.WriteLine("Hello, "+name+"!");
+
+		Console.ReadKey();
+	}
+ }
+ 
---
+++ README.md
@@ -0,0 +1,17 @@
+= Hello User by Joanne =
+
+== Welcome to my First C# Demo ==
+
+Modified from console template https://aka.ms/new-console-template.
+
+To compile, run csc command:
+
+```
+csc Program.cs
+```
+
+To run, type:
+
+```
+HelloUser
+```
